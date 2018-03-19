ee881164105cf68bf40cb11c72eb7e08ea0260a4
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-19 15:52:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ steem.api.getRepliesByLastUpdate('jeaimetu', '6c1klq-stereotype', 10, function(e
 */
 
 console.log('getDiscussionsByAuthorBeforeDate test');
-steem.api.getDiscussionsByAuthorBeforeDate('jeaimetu', '6c1klq-stereotype', '2018-03-20'T'11:55:10, 10, function(err, result) {
+steem.api.getDiscussionsByAuthorBeforeDate('jeaimetu', '6c1klq-stereotype', '2018-03-20'T'11:55:10', 10, function(err, result) {
   console.log(err, result);
 });
 
