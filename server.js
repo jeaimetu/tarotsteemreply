e81f77798682b803444c501c47a8ea424bb6f654
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-20 14:29:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function writingReply(child_permlink){
 	const permlink = steem.formatter.commentPermlink(parent_author, parent_permlink)
 	
 	var content = '<table><tr><td> . ';
-	content += '</td><td><p><strong>Hi, this is your requested tarot</strong></p><hr><p>';
+	content += '</td><td><p><strong>안녕하세요. 타로점 결과 입니다. 조만간 한글 버전도 나오니, 많이 사용해 주세요.</strong></p><hr><p>';
 	content += tarot.randomCard();
 	content += '</td></tr></table>';
 	
