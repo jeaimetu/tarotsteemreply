d10f3a7474854d5aba273f21c98d3c34bb233f7c
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-03-20 13:42:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ function writingReply(child_permlink){
 	var parent_permlink = child_permlink;
 	var json_metadata = '';
 	const permlink = steem.formatter.commentPermlink(parent_author, parent_permlink)
-	var content = "<table><tr><td><img src = https://cdn.pixabay.com/photo/2017/10/09/13/24/priest-2833384_960_720.jpg height=200 witdh = 100></td>
-<td><p><strong>Hi, this is your requested tarot</strong></p><hr><p>";
+	var content = "<table><tr><td><img src = https://cdn.pixabay.com/photo/2017/10/09/13/24/priest-2833384_960_720.jpg";
+	content += "height=200 witdh = 100></td><td><p><strong>Hi, this is your requested tarot</strong></p><hr><p>";
 	content += tarot.randomCard();
 	steem.broadcast.comment (
     	private_posting_wif,  // Steemit.com Wallet -> Permissions -> Show Private Key (for Posting)
