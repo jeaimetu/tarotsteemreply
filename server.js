55ebfa6341c3fe8db04e2d6031374457cf6ca9e9
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-04-06 19:08:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,8 @@ setInterval(checkReplies, 25000);
 steem.api.getAccountHistory('jeaimetu', -1,100, function(err, result) {
   //console.log(err, result);
 	result.forEach((num, idx)=> {
-		console.log(num.op(0), num.op(1));
+		//console.log(num.op(0), num.op(1));
+		console.log(num);
 	});
 });
 
